480a8deef184697395e4e35a4e0d473161de9e5a
RonRamal <59611500+RonRamal@users.noreply.github.com>
2020-08-22 14:36:20 +0300

    Update ToursInterdace

--- Flight/Flight/Controllers/ToursController.cs
+++ Flight/Flight/Controllers/ToursController.cs
@@ -24,11 +24,10 @@ namespace ClassEX3.Controllers
         }
 
         // POST api/<controller>
-        public List<Tours> Post([FromBody]Tours tour)
+        public int Post([FromBody]Tours tour)
         {
             Tours t = new Tours();
-            t.InsertTour(tour);
-            return null;
+            return t.InsertTour(tour);
         }
 
         // PUT api/<controller>/5
